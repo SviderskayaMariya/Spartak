f3a2a3339402f6a4512db9badc194f82b84383a3
Darya Gerasimova <darya14gerasimova@gmail.com>
2017-12-24 20:50:17 +0300

    Изменения

---
+++ Sapper/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sapper
+{
+    class Player
+    {
+        public int key;
+        public string name;
+        public DateTime bdate;
+        public int height;
+        public int weight;
+        public string nationality;
+        public string role;
+        public int number;
+        public Player(int w, string q, DateTime a, int b, int c, string d, string e, int f)
+        {
+            key = w;
+            name = q;
+            bdate = a;
+            height = b;
+            weight = c;
+            nationality = d;
+            role = e;
+            number = f;
+        }
+        public override string ToString()
+        {
+            return $"Игрок: {name.Trim()}, Дата рождения: {bdate.ToShortDateString()}, Рост: {height}, Вес: {weight}, Гражданство: {nationality}, Амплуа: {role}, Номер: {number}.";
+        }
+    }
+}
